0657dcb1cc88601f204e985e1a1df6236a99417c
서지호 <jihoseo2006@gmail.com>
2025-10-17 11:31:56 +0900

    feat: install tailwindcss & postcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>피그마 mcp 퍼블리싱 테스트</h1>
+        <h1 className="text-red-500">피그마 mcp 퍼블리싱 테스트</h1>
         <span>제제제 제제제</span>
       </div>
     </>

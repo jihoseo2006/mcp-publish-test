2b1c70058d7b97db7767fbe735cce21cc9fae80a
서지호 <jihoseo2006@gmail.com>
2025-10-28 09:33:56 +0900

    fix: v2 작업을 위한 기존 결과물 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainPage from "./pages/main-page";
-import Notification from "./pages/notification";
-import Pannel from "./pages/pannel";
-import Kfiri from "./pages/k-firi";
-import Gov from "./pages/gov";
-import Education from "./pages/education";
-import Additional from "./pages/additional";
-import Detail from "./pages/detail";
-import PannelInsert from "./pages/pannel-insert";
+
 
 
 
@@ -33,26 +25,6 @@ const router = createBrowserRouter([
     path: "/detail",
     element: <Detail />,
   },
-  {
-    path: "/k-firi",
-    element: <Kfiri />,
-  },
-  {
-    path: "/gov",
-    element: <Gov />,
-  },
-  {
-    path: "/education",
-    element: <Education />,
-  },
-  {
-    path: "/additional",
-    element: <Additional />,
-  },
-  {
-    path: "/pannel-insert",
-    element: <PannelInsert />
-  }
 ]);
 
 
--- src/pages/k-firi.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Kfiri = () => {
-  return (
-    <div>Kfiri</div>
-  )
-}
-
-export default Kfiri

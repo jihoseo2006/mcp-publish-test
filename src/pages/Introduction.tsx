486a221f54189ed1b1034803faa12abb81f9838b
서지호 <jihoseo2006@gmail.com>
2025-11-21 10:23:19 +0900

    feat: 인사말 페이지 퍼블리싱
    - Introduction 페이지 구현
    - 헤더 컴포넌트 재사용
    - 배너 이미지 추가
    - 프로필 이미지 추가
    - 탭 네비게이션 구현
    - 원장 인사말 콘텐츠 구현
    - Footer 구현

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,95 +1,82 @@
-import HamburgerIcon from "~/assets/66dabbf745f04725102281e9a3e1e6414a5f4ae7.svg?react";
-import BellOnIcon from "~/assets/58ca699f884eb8bdedfa3ed146f69a5665f87797.svg?react";
+import ProfileImage from "~/assets/73500b0a55084f30e265d527aab733b446ea6b36.png";
+import BannerImage from "~/assets/dcf333679af90eb679b4e6c0f1df43f9052773fe.png";
+import Header from "~/components/header";
 
-const Introduction = () => {
+export default function Introduction() {
   return (
-    <div className="bg-white relative w-[1440px] h-[1765px]">
-      {/* Footer */}
-      <div className="absolute left-0 top-[1574px]">
-        <div className="absolute bg-white h-[190px] left-0 top-[1575px] w-[1440px]" />
-        <div className="absolute bg-[#ececec] h-px left-1/2 top-[1574px] translate-x-[-50%] w-[1440px]" />
-        <p className="absolute font-['Pretendard'] font-light leading-[16px] left-[100px] not-italic text-[#222222] text-[10px] text-nowrap top-[1647px] whitespace-pre">
-          (사)한국외식업중앙회 한국외식산업연구원
-        </p>
-        <div className="absolute font-['Pretendard'] font-light leading-[0] left-[100px] not-italic text-[#444444] text-[10px] text-nowrap top-[1674px] whitespace-pre">
-          <p className="leading-[16px] mb-0">
-            <span className="font-['Pretendard'] font-bold not-italic">주소 </span>
-            <span> (04589) 서울특별시 중구 다산로 168 (신당동, 성원빌딩) 3층 한국외식산업연구원 </span>
-            <span className="text-[#b6b6b6]"> </span>
-            <span className="text-[#d1d1d1]">ㅣ</span>
-            <span className="font-['Pretendard'] font-bold not-italic">사업자등록번호</span>
-            <span>  203-82-32145 </span>
-            <span className="text-[#b6b6b6]"> </span>
-            <span className="text-[#d1d1d1]">ㅣ</span>
-            <span className="font-['Pretendard'] font-bold not-italic">대표자</span>
-            <span> 전강식 </span>
-          </p>
-          <p className="leading-[16px]">
-            <span className="font-['Pretendard'] font-bold not-italic">TEL  </span>
-            <span>02-6191-2908  </span>
-            <span className="text-[#d1d1d1]">ㅣ</span>
-            <span className="font-['Pretendard'] font-bold not-italic">FAX</span>
-            <span>  02-6191-2998 </span>
-            <span className="text-[#b6b6b6]"> </span>
-            <span className="text-[#d1d1d1]">ㅣ</span>
-            <span className="font-['Pretendard'] font-bold not-italic">E-mail</span>
-            <span> isaacsgod@kfiri.org </span>
-            <span className="text-[#b6b6b6]"> </span>
-            <span className="text-[#d1d1d1]">ㅣ</span>
-            <span className="font-['Pretendard'] font-bold not-italic">개인정보보호정책 책임자</span>
-            <span>  김삼희</span>
-          </p>
-        </div>
-        <p className="absolute font-['Pretendard'] font-normal leading-[15px] left-[103px] not-italic text-[#b6b6b6] text-[10px] text-nowrap top-[1721px] whitespace-pre">
-          Copyright © 2017 한국외식산업연구원 Korea food industry Research Institute All Rights Reserved.
-        </p>
-        <p className="absolute font-['Pretendard'] font-semibold leading-[12px] left-[100px] not-italic text-[#444444] text-[12px] text-nowrap top-[1600px] whitespace-pre">
-          개인정보처리방침
-        </p>
-        <p className="absolute font-['Pretendard'] font-semibold leading-[12px] left-[203px] not-italic text-[#444444] text-[12px] text-nowrap top-[1600px] whitespace-pre">
-          사이트이용약관
-        </p>
+    <div className="relative min-h-screen w-full bg-white">
+      {/* Header */}
+      <div className="h-[131px] w-full">
+        <Header />
       </div>
 
-      {/* 타이틀 */}
-      <p className="absolute font-['Pretendard'] font-bold leading-[30px] left-1/2 not-italic text-[#333333] text-[30px] text-center text-nowrap top-[219px] translate-x-[-50%] whitespace-pre">
-        인사말
-      </p>
-      <p className="absolute font-['Pretendard'] font-medium leading-[18px] left-[calc(50%-0.5px)] not-italic text-[#666666] text-[18px] text-center text-nowrap top-[191px] translate-x-[-50%] whitespace-pre">
+      <div className="h-[1px] w-full bg-[#e6e6e6]" />
+
+      {/* Breadcrumb */}
+      <p className="text-18 mt-60 text-center font-['Pretendard'] leading-[18px] font-medium text-nowrap whitespace-pre text-[#666666] not-italic">
         한국외식산업연구원
       </p>
 
-      {/* 탭 메뉴 */}
-      <div className="absolute flex gap-[40px] items-start left-[calc(50%-0.5px)] top-[287px] translate-x-[-50%]">
-        <div className="flex flex-col gap-[14px] items-start relative shrink-0">
-          <div className="flex flex-col font-['Pretendard'] font-bold justify-center leading-[0] not-italic relative shrink-0 text-[#5a33be] text-[20px] text-nowrap">
+      {/* Page Title */}
+      <p className="text-30 mt-28 text-center font-['Pretendard'] leading-30 font-bold text-nowrap whitespace-pre text-[#333333] not-italic">
+        인사말
+      </p>
+
+      {/* Tab Navigation */}
+      <div className="mt-38 flex items-start justify-center gap-40">
+        <div className="flex flex-col items-start gap-14">
+          <div className="text-20 flex flex-col justify-center font-['Pretendard'] leading-[0] font-bold text-nowrap text-[#5a33be] not-italic">
             <p className="leading-[18px] whitespace-pre">인사말</p>
           </div>
-          <div className="bg-[#5a33be] border border-[#5a33be] border-solid h-[2px] shrink-0 w-full" />
+          <div className="h-[2px] w-full border border-solid border-[#5a33be] bg-[#5a33be]" />
         </div>
-        <div className="flex flex-col font-['Pretendard'] font-normal justify-center leading-[0] not-italic relative shrink-0 text-[#888888] text-[20px] text-center text-nowrap">
+        <div className="text-20 flex flex-col justify-center text-center font-['Pretendard'] leading-[0] font-normal text-nowrap text-[#888888] not-italic">
           <p className="leading-[18px] whitespace-pre">연구원소개</p>
         </div>
-        <div className="flex flex-col font-['Pretendard'] font-normal justify-center leading-[0] not-italic relative shrink-0 text-[#888888] text-[20px] text-center text-nowrap">
+        <div className="text-20 flex flex-col justify-center text-center font-['Pretendard'] leading-[0] font-normal text-nowrap text-[#888888] not-italic">
           <p className="leading-[18px] whitespace-pre">조직도</p>
         </div>
-        <div className="flex flex-col font-['Pretendard'] font-normal justify-center leading-[0] not-italic relative shrink-0 text-[#888888] text-[20px] text-center text-nowrap">
+        <div className="text-20 flex flex-col justify-center text-center font-['Pretendard'] leading-[0] font-normal text-nowrap text-[#888888] not-italic">
           <p className="leading-[18px] whitespace-pre">주요연혁</p>
         </div>
-        <div className="flex flex-col font-['Pretendard'] font-normal justify-center leading-[0] not-italic relative shrink-0 text-[#888888] text-[20px] text-center text-nowrap">
+        <div className="text-20 flex flex-col justify-center text-center font-['Pretendard'] leading-[0] font-normal text-nowrap text-[#888888] not-italic">
           <p className="leading-[18px] whitespace-pre">찾아오시는길</p>
         </div>
       </div>
 
-      {/* 본문 텍스트 */}
-      <div className="absolute bottom-[46.86%] font-['Pretendard'] font-semibold leading-[0] left-[calc(50%-100px)] not-italic text-[#333333] text-[0px] text-nowrap top-[49.74%] whitespace-pre">
-        <p className="font-['Pretendard'] font-normal leading-[30px] mb-0 text-[20px]">
-          한국외식산업연구원은 한국외식업중앙회 소속 연구기관으로서
-        </p>
-        <p className="leading-[30px] text-[20px]">한국 외식산업의 발전을 위해 산업 경쟁력 강화에 기여해왔습니다.</p>
+      {/* Banner Image */}
+      <div className="mt-16 h-[250px] w-full">
+        <img alt="" className="h-full w-full object-cover object-center" src={BannerImage} />
       </div>
 
-      <div className="absolute bottom-[26.12%] font-['Pretendard'] font-normal leading-[28px] left-[calc(50%-100px)] not-italic text-[#333333] text-[16px] text-nowrap top-[54.84%] whitespace-pre">
+      {/* Welcome Message */}
+      <div className="text-30 mt-120 text-center font-['Pretendard'] leading-[44px] font-normal whitespace-pre text-[#333333] not-italic">
+        <p className="mb-0">안녕하십니까?</p>
+        <p className="mb-0">
+          <span className="font-['Pretendard'] font-bold text-[#333333] not-italic">한국외식산업연구원 </span>
+          홈페이지 방문을
+        </p>
+        <p>진심으로 환영합니다.</p>
+      </div>
+
+      {/* Divider */}
+      <div className="mx-auto mt-10 h-[1px] w-[1240px] bg-[#d8dde5]" />
+
+      {/* Main Content */}
+      <div className="mx-auto mt-80 w-[1240px] font-['Pretendard'] text-[0px] leading-[0] font-semibold text-nowrap whitespace-pre text-[#333333] not-italic">
+        <p className="text-20 mb-0 font-['Pretendard'] leading-30 font-normal">
+          한국외식산업연구원은 한국외식업중앙회 소속 연구기관으로서
+        </p>
+        <p className="text-20 leading-30">한국 외식산업의 발전을 위해 산업 경쟁력 강화에 기여해왔습니다.</p>
+      </div>
+
+      {/* Profile Image */}
+      <div className="mt-78 ml-[400px] h-[196px] w-[140px]">
+        <img alt="" className="h-full w-full object-cover object-center" src={ProfileImage} />
+      </div>
+
+      {/* Description */}
+      <div className="text-16 mx-auto mt-[-120px] w-[1240px] font-['Pretendard'] leading-28 font-normal whitespace-pre text-[#333333] not-italic">
         <p className="mb-0">외식산업의 산업구조 고도화 달성에 기여할 수 있는 미래비전 제시를 위해 </p>
         <p className="mb-0">관련 분야 연구의 질을 높이는 것은 물론, 끊임없는 교육과 연구를 통해</p>
         <p className="mb-0">새로운 기회를 창출해 나가고자 합니다.</p>
@@ -104,119 +91,61 @@ const Introduction = () => {
         <p>감사합니다.</p>
       </div>
 
-      {/* 배경 이미지 */}
-      <div className="absolute h-[250px] left-0 top-[341px] w-[1440px]">
-        <img 
-          alt="" 
-          className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" 
-          src="/src/assets/dcf333679af90eb679b4e6c0f1df43f9052773fe.png" 
-        />
-      </div>
-
-      {/* 원장 사진 */}
-      <div className="absolute h-[196px] left-[400px] top-[878px] w-[140px]">
-        <img 
-          alt="원장 사진" 
-          className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" 
-          src="/src/assets/73500b0a55084f30e265d527aab733b446ea6b36.png" 
-        />
-      </div>
-
-      {/* 환영 인사 */}
-      <div className="absolute bottom-[56.2%] font-['Pretendard'] font-normal leading-[44px] left-1/2 not-italic text-[#333333] text-[30px] text-center text-nowrap top-[36.32%] translate-x-[-50%] whitespace-pre">
-        <p className="mb-0">안녕하십니까?</p>
-        <p className="mb-0">
-          <span className="font-['Pretendard'] font-bold not-italic text-[#333333]">한국외식산업연구원 </span>
-          홈페이지 방문을
-        </p>
-        <p>진심으로 환영합니다.</p>
-      </div>
-
-      {/* 원장 서명 */}
-      <div className="absolute bottom-[19.72%] font-['Pretendard'] font-semibold leading-[30px] left-[calc(50%-100px)] not-italic text-[#333333] text-[0px] text-nowrap top-[76.94%] whitespace-pre">
-        <p className="font-['Pretendard'] font-normal mb-0 text-[16px]">
+      {/* Signature */}
+      <div className="mx-auto mt-60 w-[1240px] font-['Pretendard'] text-[0px] leading-30 font-semibold whitespace-pre text-[#333333] not-italic">
+        <p className="text-16 mb-0 font-['Pretendard'] font-normal">
           <span>한국외식산업연구원 원장 </span>
           <span>(조리외식경영학 박사) </span>
         </p>
-        <p className="text-[20px]">박영수</p>
+        <p className="text-20">박영수</p>
       </div>
 
-      {/* 구분선 */}
-      <div className="absolute bg-[#d8dde5] bottom-[53.31%] left-1/2 top-[46.63%] translate-x-[-50%] w-[1240px]" />
-      <div className="absolute bg-[#e6e6e6] h-px left-0 top-[130px] w-[1440px]" />
-
-      {/* Header */}
-      <div className="absolute h-[131px] right-0 top-0 w-[1440px]">
-        <div className="absolute bg-white bottom-[0.76%] left-0 right-0 top-[22.9%]" />
-        <div className="absolute bottom-[77.1%] left-0 right-0 top-0">
-          <img 
-            alt="" 
-            className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" 
-            src="/src/assets/c14f9c63fa1f29bef2b87a946a6b316938dbda6b.png" 
-          />
+      {/* Footer */}
+      <div className="mt-180">
+        <div className="h-[190px] w-full bg-white" />
+        <div className="absolute h-[1px] w-full bg-[#ececec]" />
+        <p className="text-10 mt-73 ml-100 font-['Pretendard'] leading-16 font-light text-nowrap whitespace-pre text-[#222222] not-italic">
+          (사)한국외식업중앙회 한국외식산업연구원{" "}
+        </p>
+        <div className="text-10 mt-27 ml-100 font-['Pretendard'] leading-[0] font-light text-nowrap whitespace-pre text-[#444444] not-italic">
+          <p className="mb-0 leading-16">
+            <span className="font-['Pretendard'] font-bold not-italic">주소 </span>
+            <span> (04589) 서울특별시 중구 다산로 168 (신당동, 성원빌딩) 3층 한국외식산업연구원 </span>
+            <span className="text-[#b6b6b6]"> </span>
+            <span className="text-[#d1d1d1]">ㅣ</span>{" "}
+            <span className="font-['Pretendard'] font-bold not-italic">사업자등록번호</span>
+            <span> 203-82-32145 </span>
+            <span className="text-[#b6b6b6]"> </span>
+            <span className="text-[#d1d1d1]">ㅣ</span>{" "}
+            <span className="font-['Pretendard'] font-bold not-italic">대표자</span>
+            <span> 전강식 </span>
+          </p>
+          <p className="leading-16">
+            <span className="font-['Pretendard'] font-bold not-italic">TEL </span>
+            <span> 02-6191-2908 </span>
+            <span className="text-[#d1d1d1]">ㅣ</span>{" "}
+            <span className="font-['Pretendard'] font-bold not-italic">FAX</span>
+            <span> 02-6191-2998 </span>
+            <span className="text-[#b6b6b6]"> </span>
+            <span className="text-[#d1d1d1]">ㅣ</span>{" "}
+            <span className="font-['Pretendard'] font-bold not-italic">E-mail</span>
+            <span> isaacsgod@kfiri.org </span>
+            <span className="text-[#b6b6b6]"> </span>
+            <span className="text-[#d1d1d1]">ㅣ</span>{" "}
+            <span className="font-['Pretendard'] font-bold not-italic">개인정보보호정책 책임자</span>
+            <span> 김삼희</span>
+          </p>
         </div>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_62.43%_33.59%_27.78%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          한국외식산업연구원
+        <p className="text-10 mt-27 ml-103 font-['Pretendard'] leading-[15px] font-normal text-nowrap whitespace-pre text-[#b6b6b6] not-italic">
+          Copyright © 2017 한국외식산업연구원 Korea food industry Research Institute All Rights Reserved.
         </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_55.28%_33.59%_40.35%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          외식정보
+        <p className="text-12 absolute top-[1600px] ml-100 font-['Pretendard'] leading-12 font-semibold text-nowrap whitespace-pre text-[#444444] not-italic">
+          개인정보처리방침
         </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_48.13%_33.59%_47.5%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          알림마당
+        <p className="text-12 absolute top-[1600px] ml-203 font-['Pretendard'] leading-12 font-semibold text-nowrap whitespace-pre text-[#444444] not-italic">
+          사이트이용약관
         </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_44.93%_33.59%_53.96%] leading-[14px] not-italic text-[#888888] text-[18px] text-nowrap whitespace-pre">
-          ㅣ
-        </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_36.32%_33.59%_57.15%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          패널설문조사
-        </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_29.17%_33.59%_66.46%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          정부지원
-        </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_24.17%_33.59%_73.61%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          교육
-        </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_15.97%_33.59%_78.61%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
-          부가혜택몰
-        </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[6.87%_12.71%_83.97%_84.38%] leading-[12px] not-italic text-[12px] text-nowrap text-right text-white whitespace-pre">
-          로그인/회원가입
-        </p>
-        <p className="absolute font-['Pretendard'] font-medium inset-[6.87%_6.94%_83.97%_88.68%] leading-[12px] not-italic text-[12px] text-nowrap text-right text-white whitespace-pre">
-          회원정보찾기
-        </p>
-        <div className="absolute inset-[45.04%_7.29%_24.43%_89.93%]">
-          <div className="absolute bg-[#222222] inset-[45.04%_7.29%_24.43%_89.93%] rounded-[10px]" />
-          <div className="absolute inset-[51.15%_7.85%_30.53%_90.49%] overflow-clip">
-            <div className="absolute inset-[21.77%_13.65%_21.98%_13.44%]">
-              <HamburgerIcon className="block max-w-none size-full" />
-            </div>
-          </div>
-        </div>
-        <div className="absolute inset-[45.8%_78.89%_23.66%_6.94%]">
-          <div className="absolute inset-0 overflow-hidden pointer-events-none">
-            <img 
-              alt="한국외식산업연구원 로고" 
-              className="absolute h-[119.21%] left-[-6.1%] max-w-none top-[-8.47%] w-[106.1%]" 
-              src="/src/assets/8d3493675a08ae1f4da162e87a6c546828d73bba.png" 
-            />
-          </div>
-        </div>
-        <div className="absolute left-[1240px] top-[59px]">
-          <div className="absolute bg-[#e7e7e7] border-2 border-[#e7e7e7] border-solid inset-[45.04%_11.11%_24.43%_86.11%] rounded-[10px]" />
-          <div className="absolute left-[calc(50%+540px)] top-[calc(50%+13.5px)] translate-x-[-50%] translate-y-[-50%]">
-            <div className="absolute left-[calc(50%+540px)] size-[24px] top-[calc(50%+13.5px)] translate-x-[-50%] translate-y-[-50%]">
-              <div className="absolute inset-[5%_4.17%_12.51%_20%]">
-                <div className="absolute bottom-[-5.05%] left-[-5.49%] right-0 top-0">
-                  <BellOnIcon className="block max-w-none size-full" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
 }
-
-export default Introduction

3aeea8609d5b49a3b75ab92bc46fb462606d6338
서지호 <jihoseo2006@gmail.com>
2025-11-21 10:19:43 +0900

    feat: 헤더 컴포넌트 추가
    - Header 컴포넌트 추가
    - 헤더 배경 이미지 및 로고 추가
    - 햄버거 메뉴 버튼 아이콘 추가
    - 알림 아이콘(on/off) 추가
    - Pretendard 폰트 사용
    - 네비게이션 메뉴 구조 구현

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,99 +1,92 @@
-import HamburgerIcon from "~/assets/66dabbf745f04725102281e9a3e1e6414a5f4ae7.svg?react";
-import BellOnIcon from "~/assets/58ca699f884eb8bdedfa3ed146f69a5665f87797.svg?react";
 import BellOffIcon from "~/assets/4ae7e2361918024cc8ab731fee976e15b2d744f0.svg?react";
+import BellOnIcon from "~/assets/58ca699f884eb8bdedfa3ed146f69a5665f87797.svg?react";
+import HamburgerIcon from "~/assets/66dabbf745f04725102281e9a3e1e6414a5f4ae7.svg?react";
+import Logo from "~/assets/8d3493675a08ae1f4da162e87a6c546828d73bba.png";
+import HeaderBg from "~/assets/c14f9c63fa1f29bef2b87a946a6b316938dbda6b.png";
+
+function BaseHamburgerButton({ className }: { className?: string }) {
+  return (
+    <div className={className}>
+      <div className="absolute inset-[21.77%_13.65%_21.98%_13.44%]">
+        <HamburgerIcon className="block size-full max-w-none" />
+      </div>
+    </div>
+  );
+}
 
 export default function Header() {
   return (
-    <div className="relative w-[1440px] h-[131px]">
-      {/* 배경 색상 영역 */}
-      <div className="absolute bottom-[0.76%] left-0 right-0 top-[22.9%] bg-white" />
-      
-      {/* 상단 그라데이션 배경 */}
-      <div className="absolute bottom-[77.1%] left-0 right-0 top-0">
-        <img 
-          alt="" 
-          className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" 
-          src="/src/assets/c14f9c63fa1f29bef2b87a946a6b316938dbda6b.png" 
+    <div className="relative h-[131px] w-full">
+      <div className="absolute top-[30px] right-0 bottom-1 left-0 bg-white" />
+      <div className="absolute top-0 right-0 bottom-[101px] left-0">
+        <img
+          alt=""
+          className="pointer-events-none absolute inset-0 size-full max-w-none object-cover object-center"
+          src={HeaderBg}
         />
       </div>
-
-      {/* 메뉴 항목들 */}
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_62.43%_33.59%_27.78%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_900px_44px_400px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         한국외식산업연구원
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_55.28%_33.59%_40.35%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_797px_44px_581px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         외식정보
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_48.13%_33.59%_47.5%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_694px_44px_684px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         알림마당
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_44.93%_33.59%_53.96%] leading-[14px] not-italic text-[#888888] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_648px_44px_777px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#888888] not-italic">
         ㅣ
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_36.32%_33.59%_57.15%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_524px_44px_824px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         패널설문조사
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_29.17%_33.59%_66.46%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_421px_44px_958px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         정부지원
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_24.17%_33.59%_73.61%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_349px_44px_1061px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         교육
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[55.73%_15.97%_33.59%_78.61%] leading-[14px] not-italic text-[#333333] text-[18px] text-nowrap whitespace-pre">
+      <p className="text-18 absolute inset-[73px_230px_44px_1133px] font-['Pretendard'] leading-[14px] font-medium text-nowrap whitespace-pre text-[#333333] not-italic">
         부가혜택몰
       </p>
-
-      {/* 우측 상단 링크 */}
-      <p className="absolute font-['Pretendard'] font-medium inset-[6.87%_12.71%_83.97%_84.38%] leading-[12px] not-italic text-[12px] text-nowrap text-right text-white whitespace-pre">
+      <p className="text-12 absolute inset-9 right-[183px] bottom-[110px] left-[1216px] text-right font-['Pretendard'] leading-[12px] font-medium text-nowrap whitespace-pre text-white not-italic">
         로그인/회원가입
       </p>
-      <p className="absolute font-['Pretendard'] font-medium inset-[6.87%_6.94%_83.97%_88.68%] leading-[12px] not-italic text-[12px] text-nowrap text-right text-white whitespace-pre">
+      <p className="text-12 absolute inset-9 right-[100px] bottom-[110px] left-[1278px] text-right font-['Pretendard'] leading-[12px] font-medium text-nowrap whitespace-pre text-white not-italic">
         회원정보찾기
       </p>
-
-      {/* 햄버거 메뉴 버튼 */}
-      <div className="absolute inset-[45.04%_7.29%_24.43%_89.93%]">
-        <div className="absolute bg-[#222222] inset-[45.04%_7.29%_24.43%_89.93%] rounded-[10px]" />
-        <div className="absolute inset-[51.15%_7.85%_30.53%_90.49%] overflow-clip">
-          <div className="absolute inset-[21.77%_13.65%_21.98%_13.44%]">
-            <HamburgerIcon className="block max-w-none size-full" />
-          </div>
-        </div>
+      <div className="absolute inset-[59px_105px_32px_1296px] contents">
+        <div className="rounded-10 absolute inset-[59px_105px_32px_1296px] bg-[#222222]" />
+        <BaseHamburgerButton className="absolute inset-[67px_113px_40px_1304px] overflow-clip" />
       </div>
-
-      {/* 로고 */}
-      <div className="absolute inset-[45.8%_78.89%_23.66%_6.94%]">
-        <div className="absolute inset-0 overflow-hidden pointer-events-none">
-          <img 
-            alt="한국외식산업연구원 로고" 
-            className="absolute h-[119.21%] left-[-6.1%] max-w-none top-[-8.47%] w-[106.1%]" 
-            src="/src/assets/8d3493675a08ae1f4da162e87a6c546828d73bba.png" 
+      <div className="absolute inset-[60px_1137px_31px_100px]">
+        <div className="pointer-events-none absolute inset-0 overflow-hidden">
+          <img
+            alt="한국외식산업연구원 로고"
+            className="absolute top-[-8.47%] left-[-6.1%] h-[119.21%] w-[106.1%] max-w-none"
+            src={Logo}
           />
         </div>
       </div>
-
-      {/* 알림 버튼 - On 상태 */}
-      <div className="absolute left-[1240px] top-[59px]">
-        <div className="absolute bg-[#e7e7e7] border-2 border-[#e7e7e7] border-solid inset-[45.04%_11.11%_24.43%_86.11%] rounded-[10px]" />
-        <div className="absolute left-[calc(50%+540px)] top-[calc(50%+13.5px)] translate-x-[-50%] translate-y-[-50%]">
-          <div className="absolute left-[calc(50%+540px)] size-[24px] top-[calc(50%+13.5px)] translate-x-[-50%] translate-y-[-50%]">
+      <div className="absolute top-[59px] left-[1240px] contents">
+        <div className="rounded-10 absolute inset-[59px_160px_32px_1240px] border-2 border-solid border-[#e7e7e7] bg-[#e7e7e7]" />
+        <div className="absolute top-[calc(50%+13.5px)] left-[calc(50%+540px)] contents translate-x-[-50%] translate-y-[-50%]">
+          <div className="absolute top-[calc(50%+13.5px)] left-[calc(50%+540px)] size-24 translate-x-[-50%] translate-y-[-50%]">
             <div className="absolute inset-[5%_4.17%_12.51%_20%]">
-              <div className="absolute bottom-[-5.05%] left-[-5.49%] right-0 top-0">
-                <BellOnIcon className="block max-w-none size-full" />
+              <div className="absolute top-0 right-0 bottom-[-5.05%] left-[-5.49%]">
+                <BellOnIcon className="block size-full max-w-none" />
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      {/* 알림 버튼 - Off 상태 (숨김 처리 가능) */}
-      <div className="absolute left-[1240px] top-[59px] hidden">
-        <div className="absolute bg-[#e7e7e7] border-2 border-[#e7e7e7] border-solid inset-[45.04%_11.11%_24.43%_86.11%] rounded-[10px]" />
-        <div className="absolute left-[calc(50%+540px)] top-[calc(50%+13.5px)] translate-x-[-50%] translate-y-[-50%]">
-          <div className="absolute left-[calc(50%+540px)] size-[24px] top-[calc(50%+13.5px)] translate-x-[-50%] translate-y-[-50%]">
+      <div className="absolute top-[59px] left-[1240px] contents">
+        <div className="rounded-10 absolute inset-[59px_160px_32px_1240px] border-2 border-solid border-[#e7e7e7] bg-[#e7e7e7]" />
+        <div className="absolute top-[calc(50%+13.5px)] left-[calc(50%+540px)] contents translate-x-[-50%] translate-y-[-50%]">
+          <div className="absolute top-[calc(50%+13.5px)] left-[calc(50%+540px)] size-24 translate-x-[-50%] translate-y-[-50%]">
             <div className="absolute inset-[20.83%_20%_12.51%_20%]">
               <div className="absolute inset-[-6.25%_-6.94%]">
-                <BellOffIcon className="block max-w-none size-full" />
+                <BellOffIcon className="block size-full max-w-none" />
               </div>
             </div>
           </div>
